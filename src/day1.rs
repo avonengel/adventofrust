a5e95e51711ac77aa15f65acbba3e2865fe0e791
Axel von Engel <a.vonengel@gmail.com>
2022-02-04 22:04:07 +0100

    day 1, part 1 🎉

--- src/day1.rs
+++ src/day1.rs
@@ -1,6 +1,24 @@
+
+#[allow(dead_code)]
+fn count_increases(text: &str) -> u32 {
+    let mut previous: u32 = u32::MAX;
+    let mut increases = 0;
+    for line in text.lines() {
+        let current = line.parse::<u32>().unwrap();
+        if previous < current {
+            increases = increases + 1;
+        }
+        previous = current;
+    }
+    return increases;
+}
+
 #[cfg(test)]
 mod tests {
     use indoc::indoc;
+    use std::fs::File;
+    use std::io::prelude::*;
+    
     const SAMPLE_INPUT: &str = indoc! {"199
     200
     208
@@ -14,7 +32,15 @@ mod tests {
     "};
 
     #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
+    fn it_counts_increases() {
+        assert_eq!(super::count_increases(SAMPLE_INPUT), 7)
+    }
+
+    #[test]
+    fn it_counts_increases_from_file() {
+        let mut file = File::open("src/day1/input").unwrap();
+        let mut contents = String::new();
+        assert!(file.read_to_string(&mut contents).is_ok());
+        assert_eq!(super::count_increases(&contents), 1616);
     }
 }
