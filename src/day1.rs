434ca9a016c1696a3d9287893a15ea2331951429
Axel von Engel <a.vonengel@gmail.com>
2022-02-04 22:32:28 +0100

    day 1, part 2 😮

--- src/day1.rs
+++ src/day1.rs
@@ -13,6 +13,21 @@ fn count_increases(text: &str) -> u32 {
     return increases;
 }
 
+#[allow(dead_code)]
+fn count_sliding_window_increases(text: &str) -> u32 {
+    let mut previous: u32 = u32::MAX;
+    let mut increases = 0;
+    let numbers: Vec<u32> = text.lines().map(|x| -> u32 { x.parse::<u32>().unwrap() }).collect();
+    for slice in numbers.windows(3) {
+        let current = slice.iter().sum();
+        if previous < current {
+            increases = increases + 1;
+        }
+        previous = current;
+    }
+    return increases;
+}
+
 #[cfg(test)]
 mod tests {
     use indoc::indoc;
@@ -42,5 +57,10 @@ mod tests {
         let mut contents = String::new();
         assert!(file.read_to_string(&mut contents).is_ok());
         assert_eq!(super::count_increases(&contents), 1616);
+    }
+
+    #[test]
+    fn it_counts_sliding_window_increases() {
+        assert_eq!(super::count_sliding_window_increases(SAMPLE_INPUT), 5)
     }
 }
