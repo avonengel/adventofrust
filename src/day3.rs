de9a504fcbb65298a539956ef74642d0819919c0
Axel von Engel <a.vonengel@gmail.com>
2022-03-12 21:22:27 +0100

    refactor so that input is iterated only once

--- src/day3.rs
+++ src/day3.rs
@@ -1,64 +1,61 @@
 use std::ops::Shl;
 
-fn gamma_rate(input: &str) -> u32 {
-    let line_length = input.lines()
-        .next()
-        .map(|l| { l.len() })
-        .expect("must contain at least one line");
-    let mut one_bits = vec![0; line_length];
-    let mut count = 0;
+#[derive(Debug)]
+struct BitField {
+    one_bits: Vec<u32>,
+    line_count: usize,
+}
 
-    for line in input.lines() {
-        count += 1;
-        for (idx, char) in line.chars().enumerate() {
-            match char {
-                '1' => one_bits[idx] += 1,
-                '0' => continue,
-                _ => {
-                    panic!("Expected input to contain only '0' and '1', got {}", char)
+impl BitField {
+    fn new(input: &str) -> BitField {
+        let line_length = input.lines()
+            .next()
+            .map(|l| { l.len() })
+            .expect("must contain at least one line");
+        let mut one_bits = vec![0; line_length];
+        let mut line_count = 0;
+
+        for (idx, line) in input.lines().enumerate() {
+            line_count = idx+1;
+            for (idx, char) in line.chars().enumerate() {
+                match char {
+                    '1' => one_bits[idx] += 1,
+                    '0' => continue,
+                    _ => {
+                        panic!("Expected input to contain only '0' and '1', got {}", char)
+                    }
                 }
             }
         }
+        BitField { one_bits, line_count }
     }
-    one_bits.iter().rev().enumerate().map(|(i, ones)| {
-        if ones > &(count / 2) {
-            return 1u32.shl(i)
-        }
-        0
-    }).sum()
-}
 
-fn epsilon_rate(input: &str) -> u32 {
-    let line_length = input.lines()
-        .next()
-        .map(|l| { l.len() })
-        .expect("must contain at least one line");
-    let mut one_bits = vec![0; line_length];
-    let mut count = 0;
-
-    for line in input.lines() {
-        count += 1;
-        for (idx, char) in line.chars().enumerate() {
-            match char {
-                '1' => one_bits[idx] += 1,
-                '0' => continue,
-                _ => {
-                    panic!("Expected input to contain only '0' and '1', got {}", char)
-                }
+    fn gamma_rate(&self) -> u32 {
+        self.one_bits.iter().rev().enumerate().map(|(i, ones)| {
+            if ones > &(self.line_count as u32 / 2) {
+                return 1u32.shl(i)
             }
-        }
+            0
+        }).sum()
     }
-    one_bits.iter().rev().enumerate().map(|(i, ones)| {
-        if ones <= &(count / 2) {
-            return 1u32.shl(i)
-        }
-        0
-    }).sum()
-}
 
+    fn epsilon_rate(&self) -> u32 {
+        self.one_bits.iter().rev().enumerate().map(|(i, ones)| {
+            if ones <= &(self.line_count as u32 / 2) {
+                return 1u32.shl(i)
+            }
+            0
+        }).sum()
+    }
+
+    fn power_consumption(&self) -> u32 {
+        self.epsilon_rate() * self.gamma_rate()
+    }
+}
 
 pub fn power_consumption(input: &str) -> u32 {
-    epsilon_rate(input) * gamma_rate(input)
+    let bit_field = BitField::new(input);
+    bit_field.power_consumption()
 }
 
 #[cfg(test)]
@@ -82,17 +79,17 @@ mod test {
 
     #[test]
     fn test_gamma_rate() {
-        assert_eq!(0b10110, gamma_rate(SAMPLE_INPUT))
+        assert_eq!(0b10110, BitField::new(SAMPLE_INPUT).gamma_rate())
     }
 
     #[test]
     fn test_epsilon_rate() {
-        assert_eq!(0b01001, epsilon_rate(SAMPLE_INPUT))
+        assert_eq!(0b01001, BitField::new(SAMPLE_INPUT).epsilon_rate())
     }
 
     #[test]
     fn test_power_consumption() {
-        assert_eq!(198, power_consumption(SAMPLE_INPUT))
+        assert_eq!(198, BitField::new(SAMPLE_INPUT).power_consumption())
     }
 
     #[test]
