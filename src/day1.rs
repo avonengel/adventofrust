6f3a45150caf89f672a096f6b760731bb615dfb3
Axel von Engel <a.vonengel@gmail.com>
2022-12-01 07:24:39 +0100

    prepare 2022 day 1

--- src/day1.rs
+++ src/day1.rs
@@ -1,68 +1,22 @@
-pub fn count_increases(text: &str) -> u32 {
-    let mut previous: u32 = u32::MAX;
-    let mut increases = 0;
-    for line in text.lines() {
-        let current = line.parse::<u32>().unwrap();
-        if previous < current {
-            increases += 1;
-        }
-        previous = current;
-    }
-    increases
-}
-
-pub fn count_sliding_window_increases(text: &str) -> u32 {
-    let mut previous: u32 = u32::MAX;
-    let mut increases = 0;
-    let numbers: Vec<u32> = text
-        .lines()
-        .map(|x| -> u32 { x.parse::<u32>().unwrap() })
-        .collect();
-    for slice in numbers.windows(3) {
-        let current = slice.iter().sum();
-        if previous < current {
-            increases += 1;
-        }
-        previous = current;
-    }
-    increases
-}
 
 #[cfg(test)]
 mod tests {
     use indoc::indoc;
 
-    const SAMPLE_INPUT: &str = indoc! {"199
-    200
-    208
-    210
-    200
-    207
-    240
-    269
-    260
-    263
+    const SAMPLE_INPUT: &str = indoc! {"1000
+        2000
+        3000
+
+        4000
+
+        5000
+        6000
+
+        7000
+        8000
+        9000
+
+        10000
     "};
 
-    #[test]
-    fn it_counts_increases() {
-        assert_eq!(super::count_increases(SAMPLE_INPUT), 7)
-    }
-
-    #[test]
-    fn it_counts_increases_from_file() {
-        let contents = crate::read_file_content("src/day1/input");
-        assert_eq!(super::count_increases(&contents), 1616);
-    }
-
-    #[test]
-    fn it_counts_sliding_window_increases() {
-        assert_eq!(super::count_sliding_window_increases(SAMPLE_INPUT), 5)
-    }
-
-    #[test]
-    fn it_counts_sliding_window_increases_from_file() {
-        let contents = crate::read_file_content("src/day1/input");
-        assert_eq!(super::count_sliding_window_increases(&contents), 1645)
-    }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,5 @@
 mod day1;
-mod day2;
-mod day3;
-mod day4;
 
-use crate::day3::BitField;
-use crate::day4::BingoGame;
 use std::fs::File;
 use std::io::prelude::*;
 
@@ -13,39 +8,7 @@ fn main() {
 
     println!("\nDay1:");
     let day1_input = read_file_content("src/day1/input");
-    println!("  Part 1: {}", day1::count_increases(&day1_input));
-    println!(
-        "  Part 2: {}",
-        day1::count_sliding_window_increases(&day1_input)
-    );
-
-    println!("\nDay2:");
-    let day2_input = read_file_content("src/day2/input");
-    println!("  Part 1: {}", day2::position_depth_product(&day2_input));
-    println!(
-        "  Part 2: {}",
-        day2::position_depth_product_with_aim(&day2_input)
-    );
-
-    println!("\nDay3:");
-    let day3_input = read_file_content("src/day3/input");
-    let bit_field = BitField::new(&day3_input);
-    println!(
-        "  Part 1 power consumption: {}",
-        bit_field.power_consumption()
-    );
-    println!(
-        "  Part 2 life support rating: {}",
-        bit_field.life_support_rating()
-    );
-
-    println!("\nDay4:");
-    let day4_input = read_file_content("src/day4/input");
-    let bingo_game = BingoGame::new(&day4_input);
-    println!(
-        "  Part 1 score of first winning board: {}",
-        bingo_game.first_winner_score()
-    );
+    // println!("  Part 1: {}", day1::count_increases(&day1_input));
 }
 
 fn read_file_content(path: &str) -> String {
