752c42f4805dfa741f9b8269ce110cd4a3c1459e
Axel von Engel <a.vonengel@gmail.com>
2022-12-17 18:48:22 +0100

    improve day9 performance via bench

--- src/day9.rs
+++ src/day9.rs
@@ -1,6 +1,17 @@
+extern crate test;
+
+use std::collections::HashSet;
+
+
 #[cfg(test)]
 mod tests {
+    use test::Bencher;
+
     use indoc::indoc;
+
+    use crate::{day9, read_file_content};
+
+    use super::*;
 
     const SAMPLE_INPUT: &str = indoc! {"R 4
         U 4
@@ -13,56 +24,56 @@ mod tests {
 
     #[test]
     fn part1_counts_start() {
-        assert_eq!(super::part1(""), 1);
+        assert_eq!(part1(""), 1);
     }
 
     #[test]
     fn part1_moves_right() {
-        assert_eq!(super::part1("R 3"), 3);
+        assert_eq!(part1("R 3"), 3);
     }
 
     #[test]
     fn part1_moves_up() {
-        assert_eq!(super::part1("U 3"), 3);
+        assert_eq!(part1("U 3"), 3);
     }
 
     #[test]
     fn part1_moves_down() {
-        assert_eq!(super::part1("D 3"), 3);
+        assert_eq!(part1("D 3"), 3);
     }
 
     #[test]
     fn part1_moves_left() {
-        assert_eq!(super::part1("L 3"), 3);
+        assert_eq!(part1("L 3"), 3);
     }
 
     #[test]
     fn part1_moves_diagonal_up_right() {
         let s = SAMPLE_INPUT.lines().take(2).fold(String::new(), |a, b| a + b + "\n");
-        assert_eq!(super::part1(&s), 7);
+        assert_eq!(part1(&s), 7);
     }
 
     #[test]
     fn part1_moves_diagonal_up_left() {
         let s = SAMPLE_INPUT.lines().take(3).fold(String::new(), |a, b| a + b + "\n");
-        assert_eq!(super::part1(&s), 9);
+        assert_eq!(part1(&s), 9);
     }
 
     #[test]
     fn part1_moves_diagonal_down_right() {
         let s = SAMPLE_INPUT.lines().take(5).fold(String::new(), |a, b| a + b + "\n");
-        assert_eq!(super::part1(&s), 10);
+        assert_eq!(part1(&s), 10);
     }
 
     #[test]
     fn part1_moves_diagonal_left_down() {
         let s = SAMPLE_INPUT.lines().take(7).fold(String::new(), |a, b| a + b + "\n");
-        assert_eq!(super::part1(&s), 13);
+        assert_eq!(part1(&s), 13);
     }
 
     #[test]
-    fn part1() {
-        assert_eq!(super::part1(SAMPLE_INPUT), 13);
+    fn test_part1() {
+        assert_eq!(part1(SAMPLE_INPUT), 13);
     }
 
 
@@ -76,16 +87,32 @@ mod tests {
         U 20"};
 
     #[test]
-    fn part2() {
+    fn test_part2() {
         assert_eq!(super::part2(PART2_SAMPLE), 36);
+    }
+
+    #[bench]
+    fn bench_day9_part1(b: &mut Bencher) {
+        let day9_input = read_file_content("src/day9/input.txt");
+        b.iter(|| {
+            part1(&day9_input)
+        })
+    }
+
+    #[bench]
+    fn bench_day9_part2(b: &mut Bencher) {
+        let day9_input = read_file_content("src/day9/input.txt");
+        b.iter(|| {
+            part2(&day9_input)
+        })
     }
 }
 
 pub(crate) fn part2(input: &str) -> usize {
     let mut knots = vec![(0, 0); 10];
-    let mut visited = Vec::new();
+    let mut visited = HashSet::new();
 
-    visited.push(knots.last().unwrap().to_owned());
+    visited.insert(knots.last().unwrap().to_owned());
     for instruction in input
         .lines()
         .map(str::trim)
@@ -107,9 +134,7 @@ pub(crate) fn part2(input: &str) -> usize {
                 knots[i] = new_knot_position(&knots[i - 1], &knots[i]);
             }
             let tail = knots[knots.len() - 1];
-            if !visited.contains(&tail) {
-                visited.push(tail);
-            }
+            visited.insert(tail);
         }
     }
     // dbg!(&visited);
@@ -119,8 +144,8 @@ pub(crate) fn part2(input: &str) -> usize {
 pub(crate) fn part1(input: &str) -> usize {
     let mut head = (0, 0);
     let mut tail = (0, 0);
-    let mut visited = Vec::new();
-    visited.push(tail);
+    let mut visited = HashSet::new();
+    visited.insert(tail);
     for instruction in input
         .lines()
         .map(str::trim)
@@ -138,9 +163,7 @@ pub(crate) fn part1(input: &str) -> usize {
         for _ in 0..steps {
             head = (head.0 + step_dir.0, head.1 + step_dir.1);
             tail = new_knot_position(&head, &tail);
-            if !visited.contains(&tail) {
-                visited.push(tail);
-            }
+            visited.insert(tail);
         }
     }
     // dbg!(&visited);
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+#![feature(test)]
 mod day1;
 mod day2;
 mod day3;
